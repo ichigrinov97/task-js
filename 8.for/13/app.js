f7766306d31b50abb3f9f7778e2a263ba87491b0
Ivan Chigrinov <chigrinov1998@gmail.com>
2024-12-05 19:14:29 +0300

    download a new foulder with js files

---
+++ 8.for/13/app.js
@@ -0,0 +1,16 @@
+// 13. Пользовательвводитстрокуи числоn. Повторитестрокуnразспомощьюциклаfor.
+// Входные: "hello", 3 → Результат: "hellohellohello"
+// Входные: "abc", 2 → Результат: "abcabc"
+// Входные: "xyz", 5 → Результат: "xyzxyzxyzxyzxyz"
+
+
+let str = 'hello';
+let num = '3';
+let result = '';
+
+for(let i = 0; i<num; i++){
+    result += str
+    
+    
+}
+console.log(result);
